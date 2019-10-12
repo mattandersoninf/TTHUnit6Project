7c36e17f3523633eb1e1cec44a36b222a00fd720
Matt Anderson <matt.anderson.inf@gmail.com>
2019-10-12 13:24:30 -0400

    Initialized app.js file
    Initialized app.js file

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+
+// qwerty variable
+const qwerty = document.querySelector('#qwerty');
+// phrase variable
+const phrase = document.querySelector('#phrase');
+// missed counter
+let missed = 0;
+
+
