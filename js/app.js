e003fa6f3dafc2814bde74a2aa9712ff0cd4d1c3
Matt Anderson <matt.anderson.inf@gmail.com>
2019-10-17 03:36:08 -0400

    Event listener for keyboard and check letter functions work
    Event listener for keyboard and check letter functions work

--- js/app.js
+++ js/app.js
@@ -7,15 +7,29 @@ const phrase = document.querySelector('#phrase');
 // missed counter
 let missed = 0;
 // starter overlay variable
-const start_overlay = document.querySelector('.start');
+const start_overlay = document.querySelector('#overlay');
 // Start Game button variable
 const btn__reset = document.querySelector('.btn__reset');
 // Add the event listener to the Start Game button so that when it is clicked
 // the start overlay will be hidden
-btn__reset.addEventListener("click", start_overlay.className='hidden', false);
+btn__reset.addEventListener("click", () => {
+    start_overlay.style.display = 'none';
+});
 
 // map constant
-const map = Array.prototype.map
+const map = Array.prototype.map;
+
+// All buttons on the web page keyboard
+const qwertyButtons = document.getElementsByTagName('button');
+
+for (let i = 0; i < qwertyButtons.length; i++){
+    qwertyButtons[i].addEventListener('click', () => {
+        qwertyButtons[i].classList.add("chosen");
+        qwertyButtons[i].setAttribute("disabled","true");
+        let letterFound = checkLetter(qwertyButtons[i]);
+    });
+}
+
 
 
 // Phrase array
@@ -36,12 +50,10 @@ const phrases = [
 function getRandomPhraseAsArray(arr){
     let random = Math.floor(Math.random()*arr.length)
     
-    let random_arr = map.call(arr[random], eachLetter=>{
-        return '${eachLetter}'
-    })
+    let random_arr = arr[random].split('');
 
     return random_arr;
-};
+}
 
 // Map a random phrase from the "phrases array" to the display
 function addPhraseToDisplay(arr){
@@ -50,26 +62,42 @@ function addPhraseToDisplay(arr){
         let temp_list_item = document.createElement("li");
         let temp_text_node = document.createTextNode(arr[i]);
         temp_list_item.appendChild(temp_text_node);
-        if (arr[i] !== ' '){
-            temp_list_item.className = "letter";
-        } else {
+        if (arr[i] === ' '){
             temp_list_item.className = "space";
+        } else {
+            temp_list_item.className = "letter";
         }
         phrase.appendChild(temp_list_item);
     }
 
 }
 
-function checkLetter(buttonClick){
-    let letter_children_arr = phrase.getElementsByClassName("letter");
-    for (let i = 0; i < letter_children_arr.length ; i++){
-        if (buttonClick.value == letter_children_arr[i]){
-
-        }
-    }
-
-    return null;
-}
 
 const phaseArray = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(phaseArray);
+
+
+function checkLetter(qwertyButton){
+    // ul that holds all of the letters in the hidden phrase
+    let phrase_ul = document.getElementById("phrase");
+    // init empty string to store the input letter
+    // it will get the value of the phrase letter if and only if the clicked letter is in the phrase_ul
+    let letter_tracker = '';
+    for (let i = 0; i < phrase_ul.children.length ; i++){
+        if (qwertyButton.textContent == phrase_ul.children[i].textContent) {
+            phrase_ul.children[i].classList.add("show");
+            if (letter_tracker == ''){
+                letter_tracker = qwertyButton.textContent;
+            }
+        }
+    }
+
+    if (letter_tracker !== ''){
+        return qwertyButton.textContent
+    }
+    else {
+        return null;
+    }
+}
+
+
